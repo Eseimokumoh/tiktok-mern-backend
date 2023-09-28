af9035fe38cc21732b57b78fcdc10538e8821a66
Eseimokumoh <119541134+Eseimokumoh@users.noreply.github.com>
2023-09-28 10:33:22 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 9000;
 app.use(express.json());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', '*');
   next();
 });
 
