cea545e7d8f1a2b2a9f7efa3c1ee90c1941f801a
Eseimokumoh <119541134+Eseimokumoh@users.noreply.github.com>
2023-09-28 10:09:56 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
     if (req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
         res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
-        return res.status(200).json({});
+        return res.status(200).send({});
     };
     next();
 });
