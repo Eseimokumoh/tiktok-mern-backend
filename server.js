32809433e7c2838a6b344b7a1a8eb2f0a1047dff
Eseimokumoh <119541134+Eseimokumoh@users.noreply.github.com>
2023-09-28 10:27:07 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -11,14 +11,9 @@ const port = process.env.PORT || 9000;
 // middlewares
 app.use(express.json());
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
-        return res.status(200).send({});
-    };
-    next();
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
 });
 
 
