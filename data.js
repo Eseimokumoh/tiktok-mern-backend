f64c767f039d80253254dd220a1a73f316f034c4
Eseimokumoh <119541134+Eseimokumoh@users.noreply.github.com>
2023-09-29 08:11:37 +0800

    Add files via upload

--- data.js
+++ data.js
@@ -1,14 +1,5 @@
 const data = [
     {
-        url: "https://www.tiktok.com/@trendy.soup/video/7170929061726833921",
-        channel: "ssssangha",
-        song: "Waste My Hate On You",
-        likes: "37921",
-        messages: "3121",
-        description: "This is an example description",
-        shares: "500",
-    },
-    {
         url: "https://www.tiktok.com/@funvibesfusion/video/7246783744319720711",
         channel: "ruhggedDev",
         song: "Waste My Hate On You",
@@ -16,6 +7,15 @@ const data = [
         messages: "4121",
         description: "This is an example description",
         shares: "700",
+    },
+     {
+        url: "https://www.tiktok.com/@trendy.soup/video/7170929061726833921",
+        channel: "ssssangha",
+        song: "Waste My Hate On You",
+        likes: "37921",
+        messages: "3121",
+        description: "This is an example description",
+        shares: "400",
     },
 ]
 
